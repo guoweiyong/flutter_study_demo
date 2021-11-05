c2f2f84c280778e186381a76f02b618f3a355d4c
op_guowy <op_guowy@emesscn.com.cn>
2021-11-05 10:50:26 +0800

    1、上传flutter适配问题

---
+++ hello_flutter/lib/14_screenfit/main.dart
@@ -0,0 +1,77 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'size_fit.dart';
+
+void main() {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //获取媒体查询信息对象
+    final Size screen_size = window.physicalSize/window.devicePixelRatio;
+    print("分辨率： phySicalWidth==${screen_size.width}, phySicalHeight===${screen_size.height}");
+
+
+    // TODO: implement build
+    return MaterialApp(
+      home: GYHomePage(),
+    );
+  }
+}
+
+class GYHomePage extends StatefulWidget {
+  const GYHomePage({Key? key}) : super(key: key);
+
+  @override
+  _GYHomePageState createState() => _GYHomePageState();
+}
+
+class _GYHomePageState extends State<GYHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("屏幕适配"),
+      ),
+      body: Center(
+        child: Container(
+          width: 200.0.dpx(),
+          height: 200.0.dpx(),
+          color: Colors.orange,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.pets),
+        onPressed: (){
+          //获取媒体查询信息对象
+          final mediaQueryData = MediaQuery.of(context);
+          //获取屏幕的宽度和高度
+          final kScreenWidth = mediaQueryData.size.width;
+          final kScreenHeight = mediaQueryData.size.height;
+          final phySicalWidth = window.physicalSize.width;
+          final phySicalHeight = window.physicalSize.height;
+
+          final dpr = window.devicePixelRatio;
+
+          print("屏幕的宽度===$kScreenWidth, 屏幕的高度===$kScreenHeight");
+          print("分辨率： phySicalWidth==$phySicalWidth, phySicalHeight===$phySicalHeight");
+          print("dpr=======$dpr");
+
+          // 3.状态栏的高度
+          // 有刘海的屏幕:44 没有刘海的屏幕为20
+          final statusBarHeight = mediaQueryData.padding.top;
+          // 有刘海的屏幕:34 没有刘海的屏幕0
+          final bottomHeight = mediaQueryData.padding.bottom;
+          print("状态栏height: $statusBarHeight 底部高度:$bottomHeight");
+        },
+      ),
+    );
+  }
+}
+
+
---
+++ hello_flutter/lib/14_screenfit/size_fit.dart
@@ -0,0 +1,75 @@
+
+import 'dart:ui';
+
+class GYSizeFit  {
+  double phySicalWidth = 0;
+  double phySicalHeight = 0;
+
+  double screenWidth = 0;
+  double screenHeight = 0;
+
+  double dpr = 0;
+  double px = 0;
+
+  double statusBarHeight = 0;
+  double bottomHeight = 0;
+
+  //初始化
+  static void initialize() {
+
+  }
+  //工厂方法构造函数
+  factory GYSizeFit() => _getInstance();
+
+  //instance的getter方法
+  static GYSizeFit get instance => _getInstance();
+
+  //静态变量 _instace, 存储唯一对象
+  // 这里必须要使用？ 声明可选值， 不然不可以赋值null
+  static GYSizeFit? _instance = null;
+
+  //获取对象
+  static GYSizeFit _getInstance() {
+    if (_instance == null) {
+      //使用私有构造方法来创建对象
+      _instance = GYSizeFit._internal();
+    }
+
+    return _instance!;
+  }
+
+   //私有的命名构造法方法 ,默认的构造方法将失效， 这样就隐藏了构造方法
+  //子类不能继承internal
+  //不是关键字，可定义其它名字
+  GYSizeFit._internal() {
+    // 初始化
+    //分辨率
+    phySicalWidth = window.physicalSize.width;
+    phySicalHeight = window.physicalSize.height;
+
+    // 屏幕宽高
+    screenWidth = window.physicalSize.width / window.devicePixelRatio;
+    screenHeight = window.physicalSize.height / window.devicePixelRatio;
+
+    //这里是以iphone6 为模板来适配的
+    dpr = screenWidth/750 ; // 像素点适配
+    px = screenWidth/750 * 2; // 物理宽度适配
+
+    //导航栏和底部工具栏的高度
+    statusBarHeight = window.padding.top;
+    bottomHeight = window.padding.bottom;
+  }
+}
+
+//给double写一个分类
+extension sizeFit on double {
+  //使用像素适配大小
+  double dpx() {
+    return this * GYSizeFit.instance.dpr;
+  }
+
+  // 使用px（物理宽度适配）
+  double px() {
+    return this * GYSizeFit.instance.px;
+  }
+}
